a1dbb65771ca1c6bfda867a4598af269f9032c34
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-02-06 16:44:14 +0000

    add solution to practice problem 10

---
+++ lesson_1/practice_problems/10.rb
@@ -0,0 +1,20 @@
+# PROBLEM: Modify the hash to include an age group for each person
+munsters = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandpa" => { "age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" },
+  "Marilyn" => { "age" => 23, "gender" => "female"}
+}
+
+munsters.each do |name, info|
+  if info['age'] < 18
+    info['age_group'] = 'kid'
+  elsif 17 < info['age'] && info['age'] < 65
+    info['age_group'] = 'adult'
+  else
+    info['age_group'] = 'senior'
+  end
+end
+
+p munsters
