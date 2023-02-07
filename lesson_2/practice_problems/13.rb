7f6694856cbd43a3b9485af15635b4c5c6d6bf97
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-02-07 20:30:14 +0000

    add PEDAC for practice problem 13

---
+++ lesson_2/practice_problems/13.rb
@@ -0,0 +1,19 @@
+# PROBLEM: Given the following data structure, return a new array containing the same
+# sub arrays sorted by number, but only
+# taking into consideration the odd numbers they contain
+
+arr = [[1, 6, 9], [6, 1, 7], [1, 8, 3], [1, 5, 9]]
+
+# example output
+[[1, 8, 3], [1, 5, 9], [6, 1, 7], [1, 6, 9]]
+
+# ALGORITHM
+# return a new array (returned by sort)
+# start sort loop to compare elements
+# transform elements so that they only have odd numbers
+#   loop through sub arrays
+#   for each sub array, loop through and select only odd numbers
+#
+
+def odd_sort(array)
+end
