2f166b85aea0c5567217d9ed84825e21d197bff1
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-02-07 17:33:29 +0000

    add original solution to practice problem 10

--- lesson_2/practice_problems/10.rb
+++ lesson_2/practice_problems/10.rb
@@ -2,7 +2,7 @@
 # use the map method to return a new array, identical in structure to the original
 # but the with every value incremented by 1
 
-[{a: 1}, {b: 2, c: 3}, {d: 4, e: 5, f: 6}]
+arr = [{a: 1}, {b: 2, c: 3}, {d: 4, e: 5, f: 6}]
 
 # EXAMPLES:
 [{a: 2}, {b: 3, c: 4}, {d: 5, e: 6, f: 7}]
@@ -17,6 +17,16 @@
 #   return new hash
 #   end each hash loop
 
-def increment_values(hash)
-
+def increment_hash_values(array)
+  array.map do |hash|
+    new_hash = {}
+    hash.each do |key, value|
+      new_hash[key] = value + 1
+    end
+    new_hash
+  end
 end
+
+p arr
+p increment_hash_values(arr)
+p arr
