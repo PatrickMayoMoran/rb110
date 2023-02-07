30feeeebf2fab0ea377e3bbd448f1222609eeb4c
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-02-07 17:47:17 +0000

    add solution for practice problem 11

--- lesson_2/practice_problems/11.rb
+++ lesson_2/practice_problems/11.rb
@@ -7,4 +7,10 @@
 arr = [[2], [3, 5, 7, 12], [9], [11, 13, 15]]
 
 def transform(array)
+  array.map do |sub_array|
+    sub_array.select { |num| num % 3 == 0 }
+  end
 end
+
+three_multiples = transform(arr)
+p three_multiples
