03d27af935f3ececd7569fb2eb2bff3deaed38c3
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-02-07 17:54:40 +0000

    add solution for practice problem 12

--- lesson_2/practice_problems/12.rb
+++ lesson_2/practice_problems/12.rb
@@ -11,4 +11,11 @@ arr = [[:a, 1], ['b', 'two'], ['sea', {c: 3}], [{a: 1, b: 2, c: 3, d: 4}, 'D']]
 # return hash
 
 def transform(array)
+  hash = {}
+  array.each do |sub_array|
+    hash[sub_array[0]] = sub_array[1]
+  end
+  hash
 end
+
+p transform(arr)
