aae5ee77df13af57c8a9e3a1a28049e2f15b6f6b
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-02-07 16:19:15 +0000

    add solution to practice problem 6

---
+++ lesson_2/practice_problems/6.rb
@@ -0,0 +1,15 @@
+# PROBLEM: Print out custom string with each person's data
+
+munsters = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandpa" => { "age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" },
+  "Marilyn" => { "age" => 23, "gender" => "female"}
+}
+
+munsters.each do |name, info|
+  age = info["age"]
+  gender = info["gender"]
+  puts "#{name} is a #{age}-year-old #{gender}."
+end
