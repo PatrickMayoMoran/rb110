14c572b793073a65e4e9d0abe285947292afb4b0
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-02-07 17:44:48 +0000

    add PEDAC for practice problem 11

---
+++ lesson_2/practice_problems/11.rb
@@ -0,0 +1,10 @@
+# PROBLEM: Given the following data structure, return a new array of the same
+# structure, but only with integers that are multiples of 3
+#
+# ALGORITHM
+#   transform sub arrays by selecting only elements that are multiples of 3
+
+arr = [[2], [3, 5, 7, 12], [9], [11, 13, 15]]
+
+def transform(array)
+end
