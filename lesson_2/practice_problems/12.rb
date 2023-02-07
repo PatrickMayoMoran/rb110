659af3b9ed8ff6eb89d5a409a08e0a3e99f917f0
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-02-07 17:52:55 +0000

    write out PEDAC for practice problem 12

---
+++ lesson_2/practice_problems/12.rb
@@ -0,0 +1,14 @@
+# PROBLEM Given the following data structure, and without using the Array#to_h method, write some code that will return a hash where the key is the first item in each sub array and the value is the second item.
+
+arr = [[:a, 1], ['b', 'two'], ['sea', {c: 3}], [{a: 1, b: 2, c: 3, d: 4}, 'D']]
+# expected return value: {:a=>1, "b"=>"two", "sea"=>{:c=>3}, {:a=>1, :b=>2, :c=>3, :d=>4}=>"D"}
+
+# ALGORITHM
+# Create an empty hash
+# loop through array
+#   for each sub array, assign sub_array[0] as a key and sub_array[1] as a value
+#   end loop
+# return hash
+
+def transform(array)
+end
