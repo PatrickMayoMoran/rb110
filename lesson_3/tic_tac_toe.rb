c5f07472c05224ce03f5363cdb638a33c2a65489
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-02-08 00:49:57 +0000

    add high level tic tac toe decomposition

---
+++ lesson_3/tic_tac_toe.rb
@@ -0,0 +1,20 @@
+###### TIC TAC TOE GAME #######
+
+# Game flow:
+#
+# Welcome
+#   Greeting message
+#   Rules
+#   Ready to start?
+# Who goes first?
+#   Choose first, second, or random
+#   Message about who's going first
+# LOOP of markign squares
+#   Display board
+#   Add computer/player move
+#   check for winner or board full
+#   if winner/tie, break, otherwise continue
+# Declare winner or tie
+# Ask if want to play again?
+# if yes, start over from Who goes first
+# Otherwise, end game and say goodbye
