bfdaa7c81e0775baf3f199cd396fd8b214f740c1
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-02-11 18:41:53 +0000

    refactor display board to reduce line length

--- lesson_3/tic_tac_toe.rb
+++ lesson_3/tic_tac_toe.rb
@@ -75,16 +75,22 @@ end
 def display_board(board)
   system 'clear'
   display_top(board)
-  # display_middle(board)
-  # display_bottom(board)
+  display_middle(board)
+  display_bottom(board)
+end
+
+def display_bottom(board)
+  puts "     |     |     "
+  puts "  #{board[7]}  |  #{board[8]}  |  #{board[9]}  "
+  puts "     |     |     "
+end
+
+def display_middle(board)
   puts "-----+-----+-----"
   puts "     |     |     "
   puts "  #{board[4]}  |  #{board[5]}  |  #{board[6]}  "
   puts "     |     |     "
   puts "-----+-----+-----"
-  puts "     |     |     "
-  puts "  #{board[7]}  |  #{board[8]}  |  #{board[9]}  "
-  puts "     |     |     "
 end
 
 def display_top(board)
