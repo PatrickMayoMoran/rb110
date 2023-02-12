e0c2d1d43c8930dba5ff127a7f8056f3a5dab1a8
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-02-12 03:48:13 +0000

    include brainstorming for deck and player hand data structures

--- lesson_3/twenty_one.rb
+++ lesson_3/twenty_one.rb
@@ -1,4 +1,18 @@
+# PLayers cards
+#   Array as well? Where you push cards on as you pop them off the deck?
+#   Hmmm... but then how to calculate values from cards?
+#   Cards could also be dictionaries with names and values
+#   Aces will always be an exception; need way to handle the potential
+#   point values
 # 1. Initialize deck
+### Possible decks
+# Hash with suits as keys, arrays of cards as values
+#   With this, you could randomly sample a suit and a value?
+# Array with each card being a string?
+#   Could generate deck by looping through suits and numbers to
+#   combine and create every single card
+#   Then you could "shuffle" the array for randomization and simply
+#   deal off the top
 # 2. Deal cards to player and dealer
 # 3. Player turn: hit or stay
 #   - repeat until bust or "stay"
