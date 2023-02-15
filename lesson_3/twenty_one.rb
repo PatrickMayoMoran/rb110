9d38a72b2690375a83d962ae62f7540fc4110457
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-02-15 14:57:11 +0000

    refactor calculating ace value to account for multiple aces with test cases to check

--- lesson_3/twenty_one.rb
+++ lesson_3/twenty_one.rb
@@ -104,8 +104,9 @@ end
 
 def add_ace_points(number_of_aces, total_points)
   total_points = total_points
+  buffer = number_of_aces - 1
   number_of_aces.times do
-    additional_points = total_points <= 10 ? 11 : 1
+    additional_points = total_points <= (10 - buffer) ? 11 : 1
     total_points += additional_points
   end
 
@@ -185,3 +186,28 @@ end
 #   - repeat until total >= 17
 # 6. If dealer bust, player wins.
 # 7. Compare cards and declare winner.
+#
+#### TEST CASES #####
+# Multi Ace hand
+one_ace = [["King of Hearts", 10],
+          ["Ace of Hearts", [1, 11]]
+          ]
+two_aces = [["Nine of Hearts", 9],
+          ["Ace of Hearts", [1, 11]],
+          ["Ace of Spades", [1, 11]]
+         ]
+three_aces = [["Eight of Hearts", 8],
+          ["Ace of Hearts", [1, 11]],
+          ["Ace of Spades", [1, 11]],
+          ["Ace of Clubs", [1, 11]]
+         ]
+four_aces = [["Seven of Hearts", 7],
+          ["Ace of Hearts", [1, 11]],
+          ["Ace of Spades", [1, 11]],
+          ["Ace of Clubs", [1, 11]],
+          ["Ace of Diamonds", [1, 11]]
+         ]
+p get_hand_value(one_ace)
+p get_hand_value(two_aces)
+p get_hand_value(three_aces)
+p get_hand_value(four_aces)
