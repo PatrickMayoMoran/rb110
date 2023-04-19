d93db1cc0fdcf124bf062cfde2d99ccd65df85ec
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-04-19 21:51:21 +0000

    Add frame of lettercase counter question

---
+++ small_problems/easy_7/lettercase_counter.rb
@@ -0,0 +1,10 @@
+=begin
+=end
+
+def letter_case_count(string)
+end
+
+letter_case_count('abCdef 123') == { lowercase: 5, uppercase: 1, neither: 4 }
+letter_case_count('AbCd +Ef') == { lowercase: 3, uppercase: 3, neither: 2 }
+letter_case_count('123') == { lowercase: 0, uppercase: 0, neither: 3 }
+letter_case_count('') == { lowercase: 0, uppercase: 0, neither: 0 }
