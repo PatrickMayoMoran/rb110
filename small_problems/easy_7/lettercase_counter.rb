e2c789270d302921f9fa4ad7b41f5e203bd99ef9
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-04-20 12:09:42 +0000

    Add solution to lettercase counter problem

--- small_problems/easy_7/lettercase_counter.rb
+++ small_problems/easy_7/lettercase_counter.rb
@@ -1,10 +1,26 @@
 =begin
+GOAL: Return a hash that contains the count of uppercase characters,
+lowercase characters, and other characters
 =end
 
 def letter_case_count(string)
+  lower = ("a".."z")
+  upper = ("A".."Z")
+  count = { lowercase: 0, uppercase: 0, neither: 0 }
+  string.each_char do |c|
+    if lower.include?(c)
+      count[:lowercase] += 1
+    elsif upper.include?(c)
+      count[:uppercase] += 1
+    else
+      count[:neither] += 1
+    end
+  end
+
+  count
 end
 
-letter_case_count('abCdef 123') == { lowercase: 5, uppercase: 1, neither: 4 }
-letter_case_count('AbCd +Ef') == { lowercase: 3, uppercase: 3, neither: 2 }
-letter_case_count('123') == { lowercase: 0, uppercase: 0, neither: 3 }
-letter_case_count('') == { lowercase: 0, uppercase: 0, neither: 0 }
+p letter_case_count('abCdef 123') == { lowercase: 5, uppercase: 1, neither: 4 }
+p letter_case_count('AbCd +Ef') == { lowercase: 3, uppercase: 3, neither: 2 }
+p letter_case_count('123') == { lowercase: 0, uppercase: 0, neither: 3 }
+p letter_case_count('') == { lowercase: 0, uppercase: 0, neither: 0 }
