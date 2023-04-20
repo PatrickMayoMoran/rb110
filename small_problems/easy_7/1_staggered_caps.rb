edd8d9ce6893c7a2d32af7bc39da4da502e7aa36
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-04-20 13:06:44 +0000

    Add staggered caps solution that allows alternating start

--- small_problems/easy_7/1_staggered_caps.rb
+++ small_problems/easy_7/1_staggered_caps.rb
@@ -1,9 +1,14 @@
-def staggered_case(string)
+def staggered_case(string, upper: true)
   string.chars.map.with_index do |c, i|
-    i.even? ? c.upcase : c.downcase
+    if upper
+      i.even? ? c.upcase : c.downcase
+    else
+      i.odd? ? c.upcase : c.downcase
+    end
   end.join
 end
 
 p staggered_case('I Love Launch School!') == 'I LoVe lAuNcH ScHoOl!'
 p staggered_case('ALL_CAPS') == 'AlL_CaPs'
 p staggered_case('ignore 77 the 444 numbers') == 'IgNoRe 77 ThE 444 NuMbErS'
+p staggered_case('ALL_CAPS', upper: false)
